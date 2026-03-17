38d8016aa926e9e7b70fe75057999113cc2574cc
easylearningbd <elebdsoft@gmail.com>
2026-03-18 01:23:19 +0600

    35 User Registration - API Part 2

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -33,10 +33,44 @@ export async function POST(req: NextRequest) {
         const hashedPassword = await bcrypt.hash(password,12);
 
         // Create user with Free Subscription 
-        
 
+    const user = await prisma.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name: name || null,
+            role: Role.USER,
+            image: "/default-avatar.png",
+            subscription: {
+                create: {
+                    planType: PlanType.FREE,
+                    isActive: true,
+                }, 
+            },
+            usage: {
+                create: {
+                    month: new Date().getMonth() + 1,
+                    year: new Date().getFullYear(),
+                    validationCount: 0,
+                },
+            },
+        },
+        include: {
+            subscription: true
+        },
+    });
 
+    // Remove password from resposne 
+    const { password: _, ...userWithoutPassword } = user;
+
+    return NextResponse.json(
+        {
+            message: "User created successfully",
+            user: userWithoutPassword,
+        },
+        { status: 201 }
+    );
     } catch (error) {
-        
+        console.error("Register error:", error);
     }
 }
