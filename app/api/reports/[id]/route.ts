5a8760982ac95b08aef4667ac863bf9d0f436ad9
easylearningbd <elebdsoft@gmail.com>
2026-04-06 02:10:09 +0600

    76 Reports Details Page Setup Part 3

--- app/api/reports/[id]/route.ts
+++ app/api/reports/[id]/route.ts
@@ -2,6 +2,48 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 
+
+export async function GET(
+    _req: NextRequest,{params}: {params: Promise< {id: string}>}
+) {
+
+    try {
+        const session = await auth();
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        const userId = (session.user as any).id;
+        const {id: reportId} = await params;
+
+        const report = await prisma.report.findFirst({
+            where: {
+                id: reportId ,
+                OR: [{ userId }, {isPublic: true}],
+            },
+            include: {
+                user: {
+                    select: {
+                        name: true,
+                        email: true,
+                    },
+                },
+            },
+        });
+
+    if (!report) {
+        return NextResponse.json({error: "Report not found"}, {status: 404});
+    }
+    return NextResponse.json({report});
+        
+    } catch (error) {
+        console.error("Get report error", error);
+    }
+
+}
+
+
+
 export async function DELETE(
     _req: NextRequest,{params}: {params: Promise< {id: string}>}
 ) {
