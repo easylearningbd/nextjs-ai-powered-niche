744107a1c256d92611faebb550ae117f36f48077
easylearningbd <elebdsoft@gmail.com>
2026-04-12 03:50:36 +0600

    117 Payment Request Update Status Part 3

---
+++ app/api/admin/payment-requests/[id]/route.ts
@@ -0,0 +1,86 @@
+import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { Role, } from "@prisma/client";
+
+export async function PATCH(req: NextRequest,
+    {params}: {params: Promise<{ id: string }>}
+) {
+
+ try {
+
+    const session = await auth();
+        if (!session?.user) {
+            return NextResponse.json({ error: "Unauthorized"}, { status: 401 });
+        }
+
+        // Check if user is admin
+        const userRole = (session.user as any).role;
+        if (userRole !== Role.ADMIN) {
+            return NextResponse.json({error: "Forbidden - Admin Access required"}, { status: 403});
+        }
+
+    const {id} = await params;
+    const body = await req.json();
+    const { action,rejectReason } = body;
+
+    if (!action || !["approve", "reject"].includes(action)) {
+        return NextResponse.json(
+            {error: "Invalid actions. Must be 'approve' or 'reject' "},
+            {status: 400}
+        );
+    }
+
+    if (action === "reject" && !rejectReason) {
+        return NextResponse.json(
+            {error: "Rejection reason is required "},
+            {status: 400}
+        );
+    }
+
+    const paymentRequest = await prisma.paymentRequest.findUnique({
+        where: {id},
+    });
+
+    if (!paymentRequest) {
+        return NextResponse.json(
+            {error: "Payment request is not found "},
+            {status: 404}
+        );
+    }
+
+    if (paymentRequest.status !== "PENDING") {
+         return NextResponse.json(
+            {error: "Payment request already processed "},
+            {status: 400}
+        );
+    }
+
+
+    if (action === "approve") {
+        // Update payment request status 
+        await prisma.paymentRequest.update({
+            where:{ id },
+            data: {
+                status: "APPROVED",
+                approvedAt: new Date(),
+            },
+        });
+
+    // Upgrade user to pro user
+
+
+        
+    }
+
+
+
+
+
+ } catch (error) {
+    
+ }
+
+
+
+}
